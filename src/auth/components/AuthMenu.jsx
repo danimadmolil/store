c0c02d1e778c72d5c20b07166f9bff0ca53cbb9d
daniyal <dani.madmolil@gmail.com>
2022-10-16 14:34:01 +0330

    added cart menu for authenticated user menu

--- src/auth/components/AuthMenu.jsx
+++ src/auth/components/AuthMenu.jsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import PersonAdd from "@mui/icons-material/PersonAdd";
 import Settings from "@mui/icons-material/Settings";
 import Logout from "@mui/icons-material/Logout";
-import { Grid } from "@mui/material";
+import { Badge, Button, Grid, ListItemText, Paper } from "@mui/material";
 import {
   useTheme,
   Tooltip,
@@ -18,8 +18,18 @@ import {
 import { Link } from "react-router-dom";
 import { deleteCredentialsCache } from "../../utils/localStorage";
 import { useQueryClient } from "@tanstack/react-query";
+import {
+  Delete,
+  Shop2TwoTone,
+  ShoppingCart,
+  ViewAgenda,
+  ViewAgendaOutlined,
+  Visibility,
+  Watch,
+} from "@mui/icons-material";
 
 export default function AuthMenu() {
+  const [shoppingCartRef, setShoppingCartRef] = useState(null);
   const qc = useQueryClient();
   const theme = useTheme();
   const [anchorEl, setAnchorEl] = React.useState(null);
@@ -34,6 +44,12 @@ export default function AuthMenu() {
     deleteCredentialsCache();
     qc.setQueryData(["profile"], null);
   }
+  function openShoppingCartMenu(e) {
+    setShoppingCartRef(e.target);
+  }
+  function closeShoppingCartMenu() {
+    setShoppingCartRef(null);
+  }
   return (
     <Grid
       className="bag_auth"
@@ -44,7 +60,7 @@ export default function AuthMenu() {
       sx={{
         flexWrap: "nowrap",
         marginRight: "auto",
-        width: "200px",
+        width: "auto",
         height: "44px",
         position: "relative",
         [theme.breakpoints.down("md")]: {
@@ -53,7 +69,13 @@ export default function AuthMenu() {
         },
       }}>
       <Box>
-        <Tooltip title="Account settings">
+        <Tooltip
+          sx={{
+            ".MuiPopover-root": {
+              background: "red",
+            },
+          }}
+          title="Account settings">
           <IconButton
             onClick={handleClick}
             size="small"
@@ -66,6 +88,11 @@ export default function AuthMenu() {
         </Tooltip>
       </Box>
       <Menu
+        sx={{
+          ".MuiPopover-root": {
+            background: "red",
+          },
+        }}
         anchorEl={anchorEl}
         id="account-menu"
         open={open}
@@ -102,7 +129,7 @@ export default function AuthMenu() {
         anchorOrigin={{ horizontal: "right", vertical: "bottom" }}>
         <MenuItem>
           <Link
-            to="profile"
+            to="/profile"
             style={{
               position: "absolute",
               width: "100%",
@@ -136,6 +163,80 @@ export default function AuthMenu() {
           Logout
         </MenuItem>
       </Menu>
+      <Menu
+        sx={{
+          position: "fixed",
+          overflow: "visible",
+          filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+          mt: 1.5,
+          "& .MuiAvatar-root": {
+            width: 32,
+            height: 32,
+            ml: -0.5,
+            mr: 1,
+          },
+          "&:before": {
+            content: '""',
+            display: "block",
+            position: "absolute",
+            top: 0,
+            right: 14,
+            width: 10,
+            height: 10,
+            bgcolor: "background.paper",
+            transform: "translateY(-50%) rotate(45deg)",
+            zIndex: 0,
+          },
+        }}
+        transformOrigin={{ vertical: "top", horizontal: "center" }}
+        anchorOrigin={{ horizontal: "center", vertical: "bottom" }}
+        open={Boolean(shoppingCartRef)}
+        anchorEl={shoppingCartRef}
+        onClose={closeShoppingCartMenu}>
+        <Paper sx={{ width: "300px", padding: "8px 4px" }}>
+          <MenuItem sx={{ direction: "rtl" }}>
+            <ListItemIcon>
+              <Visibility sx={{ color: "text.primary" }} />
+            </ListItemIcon>
+            <Link
+              to="/cart"
+              style={{
+                width: "100%",
+                height: "100%",
+                position: "absolute",
+                top: 0,
+                left: 0,
+                zIndex: 100,
+              }}></Link>
+            <ListItemText sx={{ textAlign: "start" }}>
+              {"مشاهده سبد خرید"}
+            </ListItemText>
+          </MenuItem>
+          <MenuItem sx={{ direction: "rtl" }}>
+            <ListItemIcon>
+              <Delete sx={{ color: "text.primary" }} />
+            </ListItemIcon>
+            <ListItemText sx={{ textAlign: "start" }}>
+              {"حذف سبد خرید"}
+            </ListItemText>
+          </MenuItem>
+          <Divider variant="middle" />
+          <Button color="primary" fullWidth variant="contained">
+            نهایی کردن خرید
+          </Button>
+        </Paper>
+      </Menu>
+      <Badge
+        overlap="circular"
+        anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+        badgeContent={2}
+        color={"primary"}>
+        <Avatar variant="circular" sizes="small">
+          <IconButton onClick={openShoppingCartMenu}>
+            <ShoppingCart />
+          </IconButton>
+        </Avatar>
+      </Badge>
     </Grid>
   );
 }
